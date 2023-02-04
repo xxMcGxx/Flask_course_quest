43b8deede0c2bba0f585fec0a9459767177fda77
Goncharov German <55760597+zak427zak@users.noreply.github.com>
2023-02-04 12:39:14 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# Конфигурация сервера Flask, т.к. проект учебный, secret_key пропишем явно и тут
+class BaseConfig:
+    SECRET_KEY = "klsfdvgdsj4ewt5vg3qweTGEWBV?%$Byghwb"
+    BOOTSTRAP_SERVE_LOCAL = True
+    DEBUG = False
